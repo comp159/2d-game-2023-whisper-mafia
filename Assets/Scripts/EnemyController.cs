177d69b5347107324d2e9bc2a9a1bad8dcd0907d
Brian Ngo <b_ngo13@u.pacific.edu>
2023-09-27 22:49:56 -0700

    Added random enemy spawning

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    // Spawn range from (0,2)
+    [SerializeField] private int heightRange = 2;
+    [SerializeField] private int widthRange = 7;
+    
+    [SerializeField] private float spawnDelay = 2f;
+    
+    [SerializeField] private int maxEnemyCount = 10;
+    private int _enemyCount = 0;
+    
+    [SerializeField] private GameObject enemy;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemy()
     {
+        // Spawn Enemy at random coords
+        var position = new Vector2(Random.Range(-widthRange, widthRange),
+            Random.Range(-heightRange + 2, heightRange + 2));
+        Instantiate(enemy, position, Quaternion.identity);
+        _enemyCount++;
         
+        // TODO Give enemy random speed in random direction
     }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (maxEnemyCount < 0 || _enemyCount < maxEnemyCount)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
 }
