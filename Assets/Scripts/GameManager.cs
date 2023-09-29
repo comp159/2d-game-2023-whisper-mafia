0906bc518ab917d8f40a79ce8a215783e96484ef
Brian Ngo <b_ngo13@u.pacific.edu>
2023-09-29 13:45:40 -0700

    Added score counting on screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI scoreCounter;
 
     private int score;
 
     public void IncreaseScore()
     {
         score++;
-        print(score);
+        scoreCounter.text = score.ToString();
     }
 
     public void ResetScore()
